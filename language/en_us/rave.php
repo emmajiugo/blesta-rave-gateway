a2ebd38ecabc489001f7f22c26f5638d35b34b56
emmajiugo <emmajiugo@gmail.com>
2019-01-18 23:26:24 +0100

    blesta billing

---
+++ language/en_us/rave.php
@@ -0,0 +1,22 @@
+<?php
+// Gateway name
+$lang['Rave.name'] = "Rave";
+
+// Settings
+$lang['Rave.live_public_key'] = 'Live Public Key';
+$lang['Rave.live_secret_key'] = 'Live Secret Key';
+$lang['Rave.test_public_key'] = 'Test Public Key';
+$lang['Rave.test_secret_key'] = 'Test Secret Key';
+$lang['Rave.live_mode'] = 'Live Mode';
+$lang['Rave.key_note'] = 'Enable the check box to go "Live".';
+
+// Process form
+$lang['Rave.buildprocess.submit'] = "Pay with Rave";
+
+// Errors
+$lang['Rave.!error.live_mode.valid'] = 'Live mode must be set to "true" if given.';
+$lang['Rave.!error.live_public_key'] = 'Please enter your live public key.';
+$lang['Rave.!error.live_secret_key'] = 'Please enter your live secret key.';
+$lang['Rave.!error.test_public_key'] = 'Please enter your test public key.';
+$lang['Rave.!error.test_secret_key'] = 'Please enter your test secret key.';
+?>
